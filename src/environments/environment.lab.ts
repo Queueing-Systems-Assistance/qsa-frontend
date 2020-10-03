30e599f9fc1e9aae86dcac9c0e16c3483207e50b
Szilágyi Zoltán <zoltan.szilagyi.cse@gmail.com>
2020-10-03 22:29:20 +0200

    QSA-23 Revert unnecessary API change

--- src/environments/environment.lab.ts
+++ src/environments/environment.lab.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'https://qsa.inf.unideb.hu/lab/api/calculator',
-    formulaApiUrl: 'http://localhost:8080'
+    formulaApiUrl: 'https://qsa.inf.unideb.hu/lab/api/formula-handler'
 }
