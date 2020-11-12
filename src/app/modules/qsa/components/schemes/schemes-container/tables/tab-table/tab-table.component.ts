2775d964f23223560de5c33caf680045703541ee
kcsanad98 <48093619+kcsanad98@users.noreply.github.com>
2020-11-12 17:57:18 +0100

    Feature/qsa 70 (#25)
    * QSA-71 calculated features are also supplied to the formula handler app

--- src/app/modules/qsa/components/schemes/schemes-container/tables/tab-table/tab-table.component.ts
+++ src/app/modules/qsa/components/schemes/schemes-container/tables/tab-table/tab-table.component.ts
@@ -44,6 +44,7 @@ export class TabTableComponent {
     }
 
     public showCalculationModal(systemFeatureId: string, systemFeatureValue: string): void {
+        console.log(this.systemTableView.systemOutputs)
         const systemId = this.systemView.id
         this.formulaBackendService.getDefaultFormula(systemFeatureId, systemId).subscribe(
             () => {
@@ -51,6 +52,8 @@ export class TabTableComponent {
                 modalRef.componentInstance.systemFeatureId = systemFeatureId
                 modalRef.componentInstance.systemId = systemId
                 modalRef.componentInstance.result = systemFeatureValue
+                modalRef.componentInstance.calculatedFeatures = this.systemTableView.systemOutputs
+                    .map(element => ({ name: element.id, value: Number.parseFloat(this.numberService.roundValue(element.values[0], 3)) }))
             },
             () => this.showErrorMessage(this.translateService.instant('noCalculationAvailable'))
         )
