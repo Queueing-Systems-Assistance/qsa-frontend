a53370c251fa386064e0c6c9d4c38d0df841840a
Csanád Kölcsei <csanad.kolcsei@nebu.local>
2020-11-22 18:56:24 +0100

    QSA-88 Added logos to main page

--- src/app/modules/qsa/components/home/logo/logo.component.ts
+++ src/app/modules/qsa/components/home/logo/logo.component.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core'
+import { AfterViewInit, Component, Input } from '@angular/core'
+import { fromEvent } from 'rxjs'
+import { distinctUntilChanged, map, share, throttleTime } from 'rxjs/operators'
 
 @Component({
     selector: 'logo',
     templateUrl: './logo.component.html',
     styles: []
 })
-export class LogoComponent {
+export class LogoComponent implements AfterViewInit {
     @Input()
-    vertical: boolean
+    sticky: boolean
+    @Input()
+    hideAtPos: number
+
+    hidden = false
+
+    ngAfterViewInit(): void {
+        const scrollPos$ = fromEvent(window, 'scroll').pipe(
+            throttleTime(10),
+            map(() => window.pageYOffset),
+            distinctUntilChanged(),
+            share()
+        )
+
+        scrollPos$.subscribe(yPos => (this.hidden = yPos >= this.hideAtPos))
+    }
 }
